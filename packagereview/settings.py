cb8b42e0cbdfc65f599b0f79e3f6e7cf03a65990
mattfreire <freire1996@gmail.com>
2020-04-13 13:54:43 +0200

    Django defender

--- packagereview/settings.py
+++ packagereview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'defender',
 ]
 
 MIDDLEWARE = [
@@ -47,14 +48,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'defender.middleware.FailedLoginMiddleware',
 ]
+
+DEFENDER_LOCKOUT_TEMPLATE = 'defender_lockout.html'
 
 ROOT_URLCONF = 'packagereview.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
